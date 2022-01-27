0fd799862e7f5f80799d84fab1e7fb9436bbf27c
Davide Vitiello <davide.vitiello33@gmail.com>
2022-01-27 12:41:02 +0100

    add capitalizeFirst function remove broken F.

--- R/routines.R
+++ R/routines.R
@@ -5,31 +5,3 @@ nav <- function(path,current_obj,stop=length(path)) {
   nav_sub(path,current_obj,stop)
 }
 
-#' #' Bind rows from dataframes in list
-#' #' @description if dataframes have different column length, return them clustered by clength (in a list)
-#' #' @param dfs_list list of dataframes
-#' #'
-#' #' @return a dataframe
-#' #' @export
-#' #' @importFrom tibble as_tibble
-#' #' @examples bindr_flxbl(list(mtcars,mtcars))
-#' bindr_flxbl <- function(dfs_list){
-#'   sapply(dfs_list,ncol) -> dfs_ncols
-#'   processed <- c()#already processed, to avoid redundancy
-#'   out <- list();i<-0
-#'   for(n in dfs_ncols){
-#'     i<-i+1
-#'     if(i %in% processed) next()
-#'
-#'     out <- tibble::as_tibble(Reduce(rbind,dfs_list[which(dfs_ncols==n)])) %>% ppend(out)
-#'     processed <- c(which(dfs_ncols==n),processed)
-#'     print(processed)
-#'   }
-#'   return(out)
-#' }
-
-
-
-
-
-
--- R/strings.R
+++ R/strings.R
@@ -3,3 +3,4 @@ ssplit <- function(stringvect,splitby=" ") strsplit(stringvect,splitby)[[1]]
 hasclmn <- function(tb,string) sum(str_detect(colnames(tb),string))
 #alternative to grepl
 grab <- function(string,delim,Nth) unlist(lapply(strsplit(string, delim, fixed = TRUE), '[', Nth))
+capitalizeFirst<- function(data) data%>%mutate(external_gene_name = sub("(.)", "\\U\\1", external_gene_name, perl=TRUE))
